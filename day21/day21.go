b631efc5e42033746c39672a3cb0f99901f24217
Ben Segall <bentekkie@google.com>
2022-12-22 09:57:50 -0500

    day21 correct

--- day21/day21.go
+++ day21/day21.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -11,7 +13,7 @@ import (
 
 func main() {
 	part1()
-	part2()
+	part2real()
 }
 
 type Monkey interface {
@@ -30,27 +32,21 @@ type MathMonkey struct {
 	lefts, rights string
 	left, right   Monkey
 	op            rune
-	calced        bool
-	n             float64
 }
 
 func (m *MathMonkey) Number() float64 {
-	if m.calced {
-		return m.n
-	}
 	leftn, rightn := m.left.Number(), m.right.Number()
 	switch m.op {
 	case '+':
-		m.n = leftn + rightn
+		return leftn + rightn
 	case '-':
-		m.n = leftn - rightn
+		return leftn - rightn
 	case '*':
-		m.n = leftn * rightn
+		return leftn * rightn
 	case '/':
-		m.n = leftn / rightn
+		return leftn / rightn
 	}
-	m.calced = true
-	return m.n
+	panic("Invalid op")
 }
 
 func part1() {
@@ -90,42 +86,80 @@ func part1() {
 	fmt.Printf("Part 1: %d\n", int(ms["root"].Number()))
 }
 
-func part2() {
+func regulaFalsi(goalFunc func(float64) (float64, error), low, high float64, maxItt int) (float64, error) {
+	var next float64
+	for i := 0; i < maxItt; i++ {
+		highFunc, err := goalFunc(high)
+		if err != nil {
+			return next, err
+		}
+		lowFunc, err := goalFunc(low)
+		if err != nil {
+			return next, err
+		}
+		next := (low*highFunc - high*lowFunc) / (highFunc - lowFunc)
+		nextFunc, err := goalFunc(next)
+		if err != nil {
+			return next, err
+		}
+		if math.Abs(low-next) <= .00001 || math.Abs(high-next) <= .00001 {
+			return next, nil
+		}
+		if (nextFunc > 0) == (lowFunc > 0) {
+			low = next
+		} else {
+			high = next
+		}
+	}
+	return next, errors.New("regula falsi failed to converge")
+}
+
+func part2real() {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	names := []string{}
-	maths := []string{}
-	nameToI := map[string]int{}
+	mathms := []*MathMonkey{}
+	ms := map[string]Monkey{}
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		if line != "" {
-			parts := strings.Split(line, ": ")
-			names = append(names, parts[0])
-			maths = append(maths, parts[1])
-			nameToI[names[len(names)-1]] = len(names) - 1
+		if line == "" {
+			continue
 		}
-	}
-	output, err := os.Create("out.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer output.Close()
-	for i, name := range names {
-		rparts := strings.Split(maths[i], " ")
-		var formula string
+		parts := strings.Split(line, ": ")
+		name := parts[0]
+		rparts := strings.Split(parts[1], " ")
 		if len(rparts) == 1 {
-			formula = rparts[0]
+			n, _ := strconv.ParseFloat(rparts[0], 64)
+			ms[name] = &NumMonkey{n}
 		} else {
-			if name == "root" {
-				rparts[1] = "-"
+			m := &MathMonkey{
+				lefts:  rparts[0],
+				rights: rparts[2],
+				op:     []rune(rparts[1])[0],
 			}
-			formula = fmt.Sprintf("B%d%sB%d", nameToI[rparts[0]]+1, rparts[1], nameToI[rparts[2]]+1)
+			mathms = append(mathms, m)
+			ms[name] = m
 		}
-		fmt.Fprintf(output, "%s,=%s\n", name, formula)
 	}
-	fmt.Printf("Part 2: Use excel solver on out.csv with target B%d=0 by changing B%d\n", nameToI["root"]+1, nameToI["humn"]+1)
+	for _, m := range mathms {
+		m.left = ms[m.lefts]
+		m.right = ms[m.rights]
+	}
+	root := ms["root"].(*MathMonkey)
+	root.op = '-'
+	me := ms["humn"].(*NumMonkey)
+	low := me.Number()
+	for root.Number() > 0 {
+		me.n *= 2
+	}
+	high := me.Number()
+	val, _ := regulaFalsi(func(in float64) (float64, error) {
+		me.n = in
+		return root.Number(), nil
+	}, low, high, 20000)
+
+	fmt.Printf("Part 2: %d\n", int(val))
 }
